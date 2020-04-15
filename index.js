61c88b5e22720e4ccc24ef243a57b5ba58388920
Carl Hallén Jansson <callehj@hotmail.se>
2020-04-15 15:34:01 +0200

    Seperate out functions and add function to upload resolvers

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 #!/usr/bin/env node
-const fetch = require("node-fetch");
 const fs = require("fs");
 const path = require("path");
+const uploadSchema = require("./lib/schema");
+const uploadFunctions = require("./lib/functions");
 require("dotenv").config();
 
 const cwd = process.cwd();
@@ -9,28 +10,17 @@ const config = JSON.parse(fs.readFileSync(path.join(cwd, ".fauna.json"), "utf8")
 
 const defaultSecret = "FAUNADB_SECRET";
 const{ schemaPath = "./models/schema.gql", secretEnv = defaultSecret } = config;
+const fnsDir = path.join(cwd, "fauna", "functions");
+const fnsFiles = fs.readdirSync(fnsDir);
+
+const fns = fnsFiles.map( file => {
+	return require(path.join(fnsDir, file));
+});
 
 const secret = process.env[secretEnv];
-const schema = path.join(cwd, schemaPath);
-const data = fs.createReadStream(schema);
 
-fetch("https://graphql.fauna.com/import", {
-	method: "POST",
-	headers: {
-		"Authorization": `Bearer ${secret}`,
-		"Content-Type": "text/plain"
-	},
-	body: data
-}).then( async res => {
-	if(!res.ok){
-		const result = await res.text();
-		console.error("Error:", result);
-		console.log();
-		console.log("Search for this error on Google:", encodeURI(`https://google.com/search?q=faunadb graphql ${result}`));
-	}
-
-	if(res.ok)
-		console.log("Successfully updated schema!!!");
-}).catch(err => {
-	throw err;
-});
+(async () => {
+	await uploadSchema(schemaPath, secret);
+	console.log();
+	await uploadFunctions(fns, secret);
+})();
