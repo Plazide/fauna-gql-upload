e4f817bff6fea2863bfec9b708bdcee312de5ef7
Carl Hallén Jansson <callehj@hotmail.se>
2020-05-19 08:46:50 +0200

    Start adding tests

---
+++ tests/unit.test.js
@@ -0,0 +1,10 @@
+const wait = require("../util/wait");
+
+test("Should wait about 500 ms", async () => {
+	const waitTime = 500;
+	const time = Date.now();
+	await wait(waitTime);
+	const newTime = Date.now();
+	expect(newTime).toBeGreaterThanOrEqual(time + waitTime);
+	expect(newTime).toBeLessThan(time + waitTime + 5);
+})
