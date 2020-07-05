1291de1cb46fd5bbd6611fd0b1431ee99a2fbc40
Carl Hallén Jansson <callehj@hotmail.se>
2020-07-05 11:37:31 +0200

    Add -y and --yes to answer yes by default on override prompt

--- index.js
+++ index.js
@@ -14,6 +14,11 @@ const argv = yargs
 		description: "Override the schema, this will delete all your data in the database.",
 		type: "boolean"
 	})
+	.option("yes", {
+		alias: "y",
+		description: "Answer yes to all potential prompts.",
+		type: "boolean"
+	})
 	.argv;
 
 const cwd = process.cwd();
