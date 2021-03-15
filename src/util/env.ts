746d86acc377773a6576f781caa8d1a474ea1450
Carl Hallén Jansson <callehj@hotmail.se>
2021-03-15 13:58:31 +0100

    Change error message to reflect correct env var

--- src/util/env.ts
+++ src/util/env.ts
@@ -14,7 +14,7 @@ const graphqlEndpoint = process.env[graphqlEndpointEnv] || "https://graphql.faun
 const secret = process.env[secretEnv];
 
 if(!secret){
-	status(`Can not read secret from environment variable "${secretEnv}".\nIf your environment file is not called ".env", you need to specify the name of it with "envPath" in ".fauna.json".\nIf you are using a different name than "FAUNADB_SECRET" for the environment variable, you need to specify that name with "secretEnv" in ".fauna.json"\n\n`, "error");
+	status(`Can not read secret from environment variable "${secretEnv}".\nIf your environment file is not called ".env", you need to specify the name of it with "envPath" in ".fauna.json".\nIf you are using a different name than "FGU_SECRET" for the environment variable, you need to specify that name with "secretEnv" in ".fauna.json"\n\n`, "error");
 
 	throw new Error("Could not read secret. More info above.");
 }
